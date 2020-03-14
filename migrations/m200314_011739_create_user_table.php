e07233663213f3325f7cc251f439cc1d4c31df8a
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2020-03-14 08:35:06 +0700

    Create User migration

--- migrations/m200314_011739_create_user_table.php
+++ migrations/m200314_011739_create_user_table.php
@@ -14,7 +14,20 @@ class m200314_011739_create_user_table extends Migration
     {
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-        ]);
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+            'username' => $this->string()->notNull(),
+            'auth_key' => $this->string(32),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string(),
+            'email' => $this->string()->notNull(),
+            'role' => $this->smallInteger()->notNull()->defaultValue(1),
+        ], $tableOptions);
+
+        $this->createIndex('idx-user-username', '{{%user}}', 'username');
+        $this->createIndex('idx-user-email', '{{%user}}', 'email');
+        $this->createIndex('idx-user-role', '{{%user}}', 'role');
+        $this->createIndex('idx-user-created_at', '{{%user}}', 'created_at');
     }
 
     /**
