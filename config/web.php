baedd6ef2292fb7111b5ebcaac80b33c5983ecfb
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2020-05-27 23:49:57 +0700

    Create User models and forms

--- config/web.php
+++ config/web.php
@@ -55,11 +55,11 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                '<_a:(login|logout|signup|email-confirm|request-password-reset|reset-password)>' => 'user/default/<_a>',
-                '<_m:[\w\-]+>/<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/<_a>',
-                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
-                '<_m:[\w\-]+>' => '<_m>/default/index',
-                '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
+//                '<_a:(login|logout|signup|email-confirm|request-password-reset|reset-password)>' => 'user/default/<_a>',
+//                '<_m:[\w\-]+>/<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/<_a>',
+//                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
+//                '<_m:[\w\-]+>' => '<_m>/default/index',
+//                '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
             ],
 
         ],
