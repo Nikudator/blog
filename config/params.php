a2d58ceb49c90dad19359dbbe9b47a012eeb269f
nikudator <nickolyan@gmail.com>
2020-05-28 12:36:49 +0700

    Config e-mail

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail' => 'nickolyan@gmail.com',
+    'senderEmail' => 'info@nikola-web.ru',
+    'senderName' => 'Nikola-WEB',
 ];
