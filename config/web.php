0dfe2c9224733e4e09c990d5ad9ab63dc224de48
nikudator <nickolyan@gmail.com>
2020-05-28 18:58:26 +0700

    Name app

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'Блог ленивого программиста',
+    'name' => 'Уголок Одноглазого Джо',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
