3151130c8e6c4c39314f071d458f900581b20a51
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2020-05-29 14:49:29 +0700

    Russian in forms

--- modules/user/views/default/resendVerificationEmail.php
+++ modules/user/views/default/resendVerificationEmail.php
@@ -22,10 +22,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+            <?= $form->field($model, 'verifyCode')->label('Код с картинки')->widget(Captcha::className(), [
                 'captchaAction' => '/user/default/captcha',
                 'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-            ])->label('Код с картинки')) ?>
+            ])) ?>
             <div class="form-group">
                 <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
             </div>
