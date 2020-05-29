2a08acf17a974021d1f48ed42e854ab3f1aeb888
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2020-05-29 16:35:13 +0000

    Config Auth

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
-$db = require __DIR__ . '/other.php';
+$auth = require __DIR__ . '/auth.php';
 
 $config = [
     'id' => 'basic',
@@ -64,7 +64,7 @@ $config = [
             ],
 
         ],
-
+	'authClientCollection' => $auth
     ],
     'params' => $params,
 ];
