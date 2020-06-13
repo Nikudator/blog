7961c2064222480d46286658a44f15a07a1aa743
nikudator <nickolyan@gmail.com>
2020-06-13 13:53:08 +0700

    Auth

--- modules/user/controllers/DefaultController.php
+++ modules/user/controllers/DefaultController.php
@@ -189,7 +189,7 @@ class DefaultController extends Controller
         /* @var $auth Auth */
         $auth = Auth::getClientId_AtributesId($client->getId(), $attributes['id']);
 
-        echo "<pre>".var_export($attributes)."</pre>"; exit;
+        echo "<pre>".var_dump($attributes)."</pre>"; exit;
 
 
 
