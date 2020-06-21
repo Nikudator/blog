605b511295c2d2d1898e049894fdb837f448c58e
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-06-21 12:33:02 +0700

    Logo

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => '<img src="/images/logo.png" class="img-responsive"/>'.Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
