717f75ca9fb6761c854c3967e011decacb12b99d
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-06-23 00:53:32 +0700

    Auth

--- modules/user/controllers/DefaultController.php
+++ modules/user/controllers/DefaultController.php
@@ -185,7 +185,22 @@ class DefaultController extends Controller
     public function onAuthSuccess($client)
     {
         $attributes = $client->getUserAttributes();
+        echo "<pre> ";
+        switch ($client->getId())
+        {
+            case 'vkontakte':
+                echo 'Вконтакте<br>';
+                break;
+            case 'yandex':
+                echo 'Яндекс<br>';
+                break;
+            case 'facebook':
+                echo 'Фэйсбук<br>';
+                break;
+        }
+        var_export($attributes);
 
+        echo "</pre>"; exit;
         /* @var $auth Auth */
         $auth = Auth::getClientId_AtributesId($client->getId(), $attributes['id']);
 
