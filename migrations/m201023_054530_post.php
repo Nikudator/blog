ab358bcfef62c502a80fc7350c3dfe6e04eb980f
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2020-10-23 05:46:08 +0000

    create post migration

---
+++ migrations/m201023_054530_post.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201023_054530_post
+ */
+class m201023_054530_post extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m201023_054530_post cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m201023_054530_post cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
