d08caca2e2043a322fb9e0ab4111b517bab980c0
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-24 22:20:55 +0700

    Bad migration

--- migrations/m200529_154112_create_auth_table.php
+++ migrations/m200529_154112_create_auth_table.php
@@ -12,12 +12,16 @@ class m200529_154112_create_auth_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%auth}}', [
             'id' => $this->primaryKey(),
             'user_id' => $this->integer()->notNull(),
             'source' => $this->string()->notNull(),
             'source_id' => $this->string()->notNull(),
-         ]);
+         ], $tableOptions);
     }
 
     /**
