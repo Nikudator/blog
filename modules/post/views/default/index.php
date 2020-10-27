cc8b3ce4f6bf0c622059386d85d25fad8ad9cb73
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-27 15:21:15 +0700

    Post

--- modules/post/views/default/index.php
+++ modules/post/views/default/index.php
@@ -24,7 +24,7 @@ use yii\widgets\LinkPager;
                     <div class="social-share">
                         <span class="social-share-title pull-left text-capitalize">Автор: <?= $post->author; ?> Опубликовано: <?= $post->getDate();?></span>
                         <ul class="text-center pull-right">
-                            <li><a class="s-facebook" href="#"><i class="fa fa-eye"></i></a></li><?= //(int) $post->viewed?>
+                            <li><a class="s-facebook" href="#"><i class="fa fa-eye"></i></a></li>
                         </ul>
                     </div>
                 </div>
