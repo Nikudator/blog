b9caa045dc949cdf01a8ec1b191b3a24cfce243f
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2020-10-27 07:07:32 +0000

    change key

---
+++ migrations/m201027_070656_change_key_post.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201027_070656_change_key_post
+ */
+class m201027_070656_change_key_post extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m201027_070656_change_key_post cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m201027_070656_change_key_post cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
