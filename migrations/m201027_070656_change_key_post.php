e21625d3c0a08d9c5b8d901d2cf712c570fc4491
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-27 14:11:10 +0700

    Post

--- migrations/m201027_070656_change_key_post.php
+++ migrations/m201027_070656_change_key_post.php
@@ -10,33 +10,20 @@ class m201027_070656_change_key_post extends Migration
     /**
      * {@inheritdoc}
      */
-    public function safeUp()
+    public function Up()
     {
+        $this->dropForeignKey(
+            '{{%fk-post-author_id}}',
+        );
 
+        $this->addForeignKey(
+            '{{%fk-post-author_id}}',
+            '{{%post}}',
+            'author_id',
+            '{{%user}}',
+            'id'
+        );
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        echo "m201027_070656_change_key_post cannot be reverted.\n";
 
-        return false;
-    }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m201027_070656_change_key_post cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
