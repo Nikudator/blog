deb5e0cd458c6197798bfa367f811220b9071b19
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-29 02:23:08 +0700

    RBAC

--- modules/user/controllers/DefaultController.php
+++ modules/user/controllers/DefaultController.php
@@ -34,9 +34,10 @@ class DefaultController extends Controller
 
     public function beforeAction($action)
     {
-
-        echo ('<pre>'.var_export($action).'</pre>');exit;
-
+        if !$action == 'login' {
+            echo('<pre>' . var_export($action) . '</pre>');
+            exit;
+        }
 
         if (parent::beforeAction($action)) {
             if (!\Yii::$app->user->can($action->id)) {
