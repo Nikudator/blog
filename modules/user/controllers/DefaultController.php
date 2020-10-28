2d0a64a9e0c11b3f7cd2d0268b0a0ab30c881edb
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-29 02:29:57 +0700

    RBAC

--- modules/user/controllers/DefaultController.php
+++ modules/user/controllers/DefaultController.php
@@ -34,13 +34,12 @@ class DefaultController extends Controller
 
     public function beforeAction($action)
     {
-
-            echo('<pre>' . var_export($action) . '</pre>');
-            exit;
-
-
         if (parent::beforeAction($action)) {
             if (!\Yii::$app->user->can($action->id)) {
+
+                echo('<pre>' . var_export($action->id) . '</pre>');
+                exit;
+
                 throw new ForbiddenHttpException('Отказано в доступе. Не достаточно прав.');
             }
             return true;
