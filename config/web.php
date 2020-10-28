f3601af80b0c7c2cc5a8468f19b1553c97cb3cb3
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-29 05:09:02 +0700

    RBAC

--- config/web.php
+++ config/web.php
@@ -87,7 +87,7 @@ $config = [
         ],
         'authManager' => [
             'class' => 'yii\rbac\PhpManager',
-            'defaultRoles' => ['root', 'admin', 'moderator', 'redactor', 'user'],
+            'defaultRoles' => ['guest'],
         ],
         'authClientCollection' => $auth
     ],
