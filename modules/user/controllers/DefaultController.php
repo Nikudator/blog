333c037b0e3e061f0209bec2c7618a8a1c963da7
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-10-29 02:21:21 +0700

    RBAC

--- modules/user/controllers/DefaultController.php
+++ modules/user/controllers/DefaultController.php
@@ -35,7 +35,7 @@ class DefaultController extends Controller
     public function beforeAction($action)
     {
 
-        echo ('<pre>'.$var_export($action).'</pre>');exit;
+        echo ('<pre>'.var_export($action).'</pre>');exit;
 
 
         if (parent::beforeAction($action)) {
