becf9ea4c787f6c7ece75dda1f386968781230d6
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-11-02 05:18:35 +0700

    RBAC update OwnPost

--- modules/post/controllers/DefaultController.php
+++ modules/post/controllers/DefaultController.php
@@ -35,9 +35,9 @@ class DefaultController extends Controller
     public function beforeAction($action)
     {
         if (parent::beforeAction($action)) {
-//echo '<pre>'.var_export($action).'</pre>'; exit;
+echo '<pre>'.var_export($this).'</pre>'; exit;
             if ($action->id === 'update') {
-                if (!\Yii::$app->user->can('updateOwnPost', ['author_id' => $this->model->author_id])) {
+                if (!\Yii::$app->user->can('updateOwnPost', ['author_id' => $this->Author])) {
                     throw new ForbiddenHttpException('Access denied');
                 }
             }
