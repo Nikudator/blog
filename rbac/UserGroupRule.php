e4939a8e4582fe72b8183f41d355e97df521e653
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-11-02 02:24:11 +0700

    RBAC

--- rbac/UserGroupRule.php
+++ rbac/UserGroupRule.php
@@ -26,7 +26,7 @@ class UserGroupRule extends Rule
             }
         }
         else{
-            return $group == 'root' || $group == 'admin' || $group == 'moderator' || $group == 'redactor' || $group == 'user' || $item->name === 'guest';
+            return $group == 'root' || $group == 'admin' || $group == 'moderator' || $group == 'redactor' || $group == 'user' || $item->name == 'guest';
         }
         return false;
     }
