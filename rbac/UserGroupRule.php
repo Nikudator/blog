fe66e1c5761b9511059a0bc83e8cd8c523c40e34
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-11-02 01:43:49 +0700

    RBAC

--- rbac/UserGroupRule.php
+++ rbac/UserGroupRule.php
@@ -26,7 +26,7 @@ class UserGroupRule extends Rule
             }
         }
         else {
-            return $item->name == 'guest';
+            return $group == 'guest';
         }
         return false;
     }
