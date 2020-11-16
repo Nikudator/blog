5aa7561fb6092438de8efb5b3b3d7dc485cdb28c
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-11-16 13:52:39 +0700

    Крошки

--- modules/post/views/default/create.php
+++ modules/post/views/default/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\post\models\Post */
 
-$this->title = 'Новая стстья';
+$this->title = 'Новая статья';
 //$this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
