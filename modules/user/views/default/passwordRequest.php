ba27755d8847826ae65c296741d6bd43587e3892
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2020-11-17 23:23:50 +0700

    RBAC

--- modules/user/views/default/passwordRequest.php
+++ modules/user/views/default/passwordRequest.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-request-password-reset">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+    <p>Введите ваш адрес электронной почты, с которым вы регистрировались на нашем сайте. Мы отправим на него ссылку для восстановления пароля.</p>
 
     <div class="row">
         <div class="col-lg-5">
