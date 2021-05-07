287688ab55f4921b218b92d5a39483f57ec7a16c
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2021-05-08 06:30:31 +0700

    Agreement

--- views/site/agreement.php
+++ views/site/agreement.php
@@ -1,1 +1,15 @@
 <?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Пользовательское соглашение';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-agreement">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <p>Тут будут типа правила.
+    <p>Не нарушайте их.
+    <p>
+</div>
