571b55a1683362554f4b91f28781b62d20d3c821
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2021-05-24 21:51:01 +0700

    Menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,9 +30,47 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-<?php
-    require '../menu.php';
-?>
+    <?php
+    NavBar::begin([
+        'brandLabel' => Html::img('@web/images/logo.png', ['alt'=>Yii::$app->name, 'title'=>Yii::$app->name, 'class'=>"img-responsive",], ),
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'encodeLabels' => false,
+        'items' => array_filter([
+            Yii::$app->user->can('create') ?
+                ['label' => Icon::show('feather-alt', ['class'=>'fa-lg']), 'url' => ['/post/create'], 'options' => ['title' => 'Создать запись']] : false,
+            ['label' => Icon::show('envelope', ['class'=>'fa-lg']), 'url' => ['/site/contact'].' Контакты', 'options' => ['title' => 'Контакты']],
+            ['label' => Icon::show('info-circle', ['class'=>'fa-lg']), 'url' => ['/site/about'].' Обо мне', 'options' => ['title' => 'Обо мне']],
+            ['label' => Icon::show('scroll', ['class'=>'fa-lg']), 'url' => ['/site/agreement'].' Правила', 'options' => ['title' => 'Правила']],
+            Yii::$app->user->isGuest ?
+            ['label' => Icon::show('user-plus', ['class'=>'fa-lg']).' Регистрация', 'url' => ['/user/default/signup'], 'options' => ['title' =>'Зарегистрироваться']] : false,
+            //Yii::$app->user->isGuest ?
+            //['label' => Icon::show('key', ['class'=>'fa-lg']), 'url' => ['/password-request'].' Контакты', 'options' => ['title' =>'Восстановить пароль']] : false,
+            Yii::$app->user->isGuest ?
+            (['label' => Icon::show('door-open', ['class'=>'fa-lg']).' Войти', 'url' => ['/login'], 'options' => ['title' => 'Войти на сайт']]
+            ) : (
+
+
+                '<li>'
+                . Html::beginForm(['/logout'], 'post')
+                . Html::submitButton(
+                    Icon::show('door-closed', ['class'=>'fa-lg']).' Выйти (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout',
+                        'title' =>'Выйти']
+                )
+                . Html::endForm()
+                . '</li>'
+            )
+        ])
+    ]);
+    NavBar::end();
+    ?>
+
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
