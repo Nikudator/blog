ab410070be643751b46f3924788f814650b1d652
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2021-06-06 01:29:11 +0700

    Youtube to Post

--- modules/post/views/default/index.php
+++ modules/post/views/default/index.php
@@ -27,14 +27,11 @@ use yii\helpers\Html;
                         <?= empty(trim(Html::encode($post->youtube))) ? false : '<div id="ytplayer'.$post->id.'"></div>' ;?>
 
                         <script>
-                            // Load the IFrame Player API code asynchronously.
                             var tag = document.createElement('script');
                             tag.src = "https://www.youtube.com/player_api";
                             var firstScriptTag = document.getElementsByTagName('script')[0];
                             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-                            // Replace the 'ytplayer' element with an <iframe> and
-                            // YouTube player after the API code downloads.
                             var player;
                             function onYouTubePlayerAPIReady() {
                                 player = new YT.Player('ytplayer<?=$post->id;?>', {
