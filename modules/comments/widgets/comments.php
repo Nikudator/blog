9b5f92a27574513096fa40f579219b90cb2bb6af
Nikolai A. Kuropiatnikov <nickolyan@gmail.com>
2021-07-08 03:00:43 +0700

    Wiget comments

--- modules/comments/widgets/comments.php
+++ modules/comments/widgets/comments.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use app\modules\comments\models\Comments;
 
-class CommentsWidget extends Widget
+class Comments extends Widget
 {
     public $master_id;
 
