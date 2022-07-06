4441be8771ab1cf0838f686673da85c8bc26fe44
NIKOLA-HOME\nicko <nickolyan@gmail.com>
2022-07-06 14:46:33 +0700

    About

--- views/site/about.php
+++ views/site/about.php
@@ -10,7 +10,11 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-    <p>Приветствую на моем сайте. Здесь я буду рассказывать и показывать всякое, что взбредет в мою голову.
+    <p>Приветствую на моем сайте. Я блогер. Здесь я буду рассказывать и показывать всякое, что взбредет в мою голову.
     <p>Возможно это будет кому то интересно.
     <p>
+    <p>Мой Youtube-канал - https://www.youtube.com/channel/UC6k9vQrMLnZPslhC7kUFhqg
+    <p>Моя группа Вконтакте - https://vk.com/oneeyedjoe
+    <p>Задонатить на Boosty - https://boosty.to/oej
+    <p>Задонатить на - https://www.donationalerts.com/r/one_eyed_joe
 </div>
